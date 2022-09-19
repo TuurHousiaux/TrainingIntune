94c9b58af87822d9452eebe00686aefd92426808
TuurHousiaux <74181606+TuurHousiaux@users.noreply.github.com>
2022-09-19 21:33:39 +0200

    Add files via upload

---
+++ hardwaredetection.ps1
@@ -0,0 +1,6 @@
+﻿md c:\HWID 
+Set-Location c:\HWID 
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted -Force 
+Install-Script -Name Get-WindowsAutopilotInfo -Force 
+$env:Path += ";C:\Program Files\WindowsPowerShell\Scripts" 
+Get-WindowsAutopilotInfo.ps1 -OutputFile AutopilotHWID.csv 
