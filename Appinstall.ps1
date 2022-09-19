4891e5a67032fd8139e98692c947eb50720df674
TuurHousiaux <74181606+TuurHousiaux@users.noreply.github.com>
2022-09-19 21:32:30 +0200

    Add files via upload

---
+++ Appinstall.ps1
@@ -0,0 +1,10 @@
+﻿param(
+    [string]$package = "",
+    [switch]$uninstall = $false
+)
+
+if ($uninstall) {
+    winget uninstall $package -h 
+} else {
+    winget install $package -h --accept-package-agreements --accept-source-agreements
+}
---
+++ detection_7Zip.ps1
@@ -0,0 +1,12 @@
+﻿####### Geef hier de juiste package naam mee ########
+
+$Package = "7zip.7zip"
+
+#####################################################
+# Verander hieronder niets
+#####################################################
+
+$winget_Exist = winget list --id $Package --exact
+    if ($winget_Exist -like "*$Package*"){
+    Write-Host "Found it!"
+}
